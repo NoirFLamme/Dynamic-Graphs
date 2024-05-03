ed52f19fe1fc55eb07d0845cead8ad9d6bd00fe4
Ramy <ramyfetteha@gmail.com>
2024-05-03 21:59:14 +0300

    Added Graph Base Class

---
+++ Server/src/Graph.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Graph {
+    private HashMap<Integer, List<Integer>> graph;
+
+    public void addNode(Integer id){
+        graph.putIfAbsent(id, new ArrayList<>());
+    }
+
+    public void removeNode(Integer id){
+        graph.values().stream().forEach(e -> e.remove(id));
+        graph.remove(id);
+    }
+
+    public void addEdge(Integer id1, Integer id2){
+        graph.get(id1).add(id2);
+        graph.get(id2).add(id1);
+    }
+
+    public void removeEdge(Integer id1, Integer id2){
+        List<Integer> e1 = graph.get(id1);
+        List<Integer> e2 = graph.get(id2);
+
+        if (e1 != null) {
+            e1.remove(id2);
+        }
+        else if(e2 != null){
+            e2.remove(id1);
+        }
+    }
+
+    public void findShortestPath(Integer id1, Integer id2){
+        // TODO: Pick A Good Algorithm, Stating Why.
+    }
+}
