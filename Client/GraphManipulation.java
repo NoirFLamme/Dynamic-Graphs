dc82d33d8f82a67f737730bcf7d0d6a56afe5c1a
Ramy <ramyfetteha@gmail.com>
2024-05-16 00:21:10 +0300

    Fixed Client Log Folder Creation

--- Client/GraphManipulation.java
+++ Client/GraphManipulation.java
@@ -1,7 +1,10 @@
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class GraphManipulation {
 	public static void main(String args[]) {
@@ -16,7 +19,7 @@ public class GraphManipulation {
 	}
 
 	private static void createClientLog(){
-		String directoryPath = "logs/client";
+		String directoryPath = "logs\\client";
 
         // Create a Path object representing the directory
         Path dirPath = Paths.get(directoryPath);
@@ -25,7 +28,21 @@ public class GraphManipulation {
             // Check if the directory already exists
             if (Files.exists(dirPath)) {
                 // If it exists, delete it and create a new one
-                Files.deleteIfExists(dirPath);
+                Files.walkFileTree(dirPath, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    System.out.println("Deleted file: " + file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    Files.delete(dir);
+                    System.out.println("Deleted directory: " + dir);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
                 Files.createDirectory(dirPath);
                 System.out.println("Directory overwritten successfully.");
             } else {
